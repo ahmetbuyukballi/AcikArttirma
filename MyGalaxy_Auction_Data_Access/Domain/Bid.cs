6017b9aa63173295b0fa6354087b189b348d8c73
ahmetbuyukballi <ahmetbuyukballi@gmail.com>
2024-10-24 20:22:29 +0300

    Add project files.

---
+++ MyGalaxy_Auction_Data_Access/Domain/Bid.cs
@@ -0,0 +1,25 @@
+﻿using MyGalaxy_Auction_Data_Access.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyGalaxy_Auction_Data_Access.Enum;
+
+namespace MyGalaxy_Auction_Data_Access.Domain
+{
+    public class Bid
+    {
+        [Key]
+        public int BidId { get; set; }
+        public decimal BidAmount { get; set; }
+        public DateTime BidDate { get; set; }
+        public string BidStatus { get; set; }=Enum.BıdStatus.Pending.ToString();
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public int VehicleId { get; set; }
+        public Vehicle Vehicle { get; set; }
+    }
+}
---
+++ MyGalaxy_Auction_Data_Access/Enum/BıdStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGalaxy_Auction_Data_Access.Enum
+{
+    public enum BıdStatus
+    {
+        Accepted,
+        Rejected,
+        Pending,
+    }
+}
